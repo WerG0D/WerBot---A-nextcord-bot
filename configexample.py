b1a2951d194d1dc2c98178f0b81fdd07061d26f0
Wer <davigwerneck@gmail.com>
2022-09-23 12:50:58 -0300

    ..

--- configexample.py
+++
@@ -1,4 +0,0 @@
-#Para usar este bot, crie um arquivo como esse chamado "config.py" e substitua os dados:
-TOKEN = "blablablatoken" #O token do seu bot vai aqui
-PREFIX = ";" # O prefixo que seu bot vai usar
-OWNER = "Wer#5376" #O nome do dono 
